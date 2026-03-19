c7ec1b1c01ebb0e2d0263a9e12e748564bff00d8
CrazyJorge <jorge.aguiar.resende@gmail.com>
2026-03-19 14:05:52 +0000

    2nd question done!

--- 02_count.py
+++ 02_count.py
@@ -14,7 +14,8 @@ def count_positive(lst):
     """Return how many integers in lst are greater than zero."""
     count = 0
     for item in lst:
-        pass  # if item is positive, increment count
+        if item > 0:
+            count += 1
     return count
 
 
@@ -22,7 +23,8 @@ def count_short(lst):
     """Return how many strings in lst have length <= 3."""
     count = 0
     for word in lst:
-        pass  # if word is short enough, increment count
+        if len(word) <= 3:
+            count += 1
     return count
 
 
@@ -48,7 +50,7 @@ def test_short_all():
     assert count_short(["a", "be", "cup"]) == 3
 
 def test_short_none():
-    assert count_short(["long", "words", "only"]) == 1
+    assert count_short(["long", "words", "only"]) == 0
 
 def test_short_empty():
     assert count_short([]) == 0
